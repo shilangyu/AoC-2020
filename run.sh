e18ecf9ef6d29f8e04ea23d8364d14e84c697a10
shilangyu <xmarcinmarcin@gmail.com>
2020-12-14 13:42:15 +0100

    CI test

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-cargo eval src/day01.rs
-go run src/day02.go
-julia src/day03.jl
-deno run --allow-read --quiet src/day04.ts
-python src/day05.py
-gcc src/day06.c -Wall -o temp.out && ./temp.out && rm temp.out
-nim r src/day07.nim 2>/dev/null
-g++ src/day08.cpp -Wall -o temp.out && ./temp.out && rm temp.out
-dart run src/day09.dart
-dotnet script src/day11.cs
-swift src/day12.swift
-cargo +nightly eval src/day13.rs
+cargo eval src/day01.rs &&
+go run src/day02.go &&
+julia src/day03.jl &&
+deno run --allow-read --quiet src/day04.ts &&
+python src/day05.py &&
+gcc src/day06.c -Wall -o temp.out && ./temp.out && rm temp.out &&
+nim r src/day07.nim 2>/dev/null &&
+g++ src/day08.cpp -Wall -o temp.out && ./temp.out && rm temp.out &&
+dart run src/day09.dart &&
+dotnet script src/day11.cs &&
+swift src/day12.swift &&
+cargo +nightly eval src/day13.rs &&
 go run src/day14.go
